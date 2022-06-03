068c1bd77858d307bc3d8ba2e7a54dea03b1f863
alvalenda <banysan@msn.com>
2022-06-03 18:52:43 -0300

    Refatorando Arquivos de Projeto

--- Modulo_1/Projetos/Projeto_2_Jokenpo.js
+++ Modulo_1/Projetos/Projeto_2_Jokenpo.js
@@ -1,5 +1,4 @@
-/*  Flávio Alvarenga - Turma C017
-    Projeto 2 - Jokenpô 
+/*  Projeto 2 - Jokenpô 
     O Jokenpô é o popular jogo do "pedra, papel e tesoura". A premissa é simples, você tem 3 elementos para escolher, e cada um deles vence de um, e perde para o outro.
     * Pedra ganha da tesoura, mas perde para o papel;
     * Tesoura ganha do papel, mas perde para a pedra;
