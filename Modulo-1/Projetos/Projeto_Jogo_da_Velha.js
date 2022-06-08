89dd7e26b3a82b0d2ed6e37edce5dd523d4911a3
alvalenda <banysan@msn.com>
2022-06-08 18:05:38 -0300

    alteracoes finais

--- Modulo-1/Projetos/Projeto_Jogo_da_Velha.js
+++ Modulo-1/Projetos/Projeto_Jogo_da_Velha.js
@@ -71,20 +71,72 @@ function novaRodada(matriz, vencedor, placar) {
 }
 
 function lerPosicao(num) {
-    if (num < 3) return [0, num];
-    else if (num < 6) return [1, num - 3];
-    else return [2, num - 6];
+    return [Math.floor((num - 1) / 3), (num - 1) % 3];
+}
+
+function darPosicao(i, j) {
+    return 1 + 3 * i + j;
+}
+
+function checaAI(matriz) {
+    let sequencia = '';
+    //  AI Checa para garantir VITÓRIA
+    for (let i = 0; i < matriz.length; i++) {
+        sequencia += matriz[i][0] + matriz[i][1] + matriz[i][2];
+        if (sequencia === 'OO ' || sequencia === 'O O' || sequencia === ' OO')
+            return darPosicao(i, sequencia.indexOf(' '));
+        else sequencia = '';
+    }
+    for (let j = 0; j < matriz.length; j++) {
+        sequencia += matriz[0][j] + matriz[1][j] + matriz[2][j];
+        if (sequencia === 'OO ' || sequencia === 'O O' || sequencia === ' OO')
+            return darPosicao(sequencia.indexOf(' '), j);
+        else sequencia = '';
+    }
+
+    sequencia = matriz[0][0] + matriz[1][1] + matriz[2][2];
+    if (sequencia === 'OO ' || sequencia === 'O O' || sequencia === ' OO')
+        return darPosicao(sequencia.indexOf(' '), sequencia.indexOf(' '));
+    else sequencia = '';
+    sequencia = matriz[0][2] + matriz[1][1] + matriz[2][0];
+    if (sequencia === ' OO' || sequencia === 'O O' || sequencia === 'OO ')
+        return darPosicao(sequencia.indexOf(' '), 2 - sequencia.indexOf(' '));
+    else sequencia = '';
+
+    //  AI Checa para evitar DERROTA
+    for (let i = 0; i < matriz.length; i++) {
+        sequencia += matriz[i][0] + matriz[i][1] + matriz[i][2];
+        if (sequencia === 'XX ' || sequencia === 'X X' || sequencia === ' XX')
+            return darPosicao(i, sequencia.indexOf(' '));
+        else sequencia = '';
+    }
+    for (let j = 0; j < matriz.length; j++) {
+        sequencia += matriz[0][j] + matriz[1][j] + matriz[2][j];
+        if (sequencia === 'XX ' || sequencia === 'X X' || sequencia === ' XX')
+            return darPosicao(sequencia.indexOf(' '), j);
+        else sequencia = '';
+    }
+
+    sequencia = matriz[0][0] + matriz[1][1] + matriz[2][2];
+    if (sequencia === 'XX ' || sequencia === 'X X' || sequencia === ' XX')
+        return darPosicao(sequencia.indexOf(' '), sequencia.indexOf(' '));
+    else sequencia = '';
+    sequencia = matriz[0][2] + matriz[1][1] + matriz[2][0];
+    if (sequencia === ' XX' || sequencia === 'X X' || sequencia === 'XX ')
+        return darPosicao(sequencia.indexOf(' '), 2 - sequencia.indexOf(' '));
+
+    return null;
 }
 
 function validaPosicao(num, matriz) {
-    const [i, j] = lerPosicao(num - 1);
+    const [i, j] = lerPosicao(num);
     if (matriz[i][j] === ' ') return false;
     else return true;
 }
 
 function computadorJoga(matriz) {
     while (true) {
-        const _jogada = Math.floor(Math.random() * 9 + 1);
+        const _jogada = checaAI(matriz) || Math.floor(Math.random() * 9 + 1);
         if (!validaPosicao(_jogada, matriz)) return _jogada;
     }
 }
@@ -118,14 +170,14 @@ function jogarNovamente() {
 }
 
 function escreveJogada(num, matriz, marca) {
-    const [i, j] = lerPosicao(num - 1);
+    const [i, j] = lerPosicao(num);
     matriz[i][j] = marca;
 }
 
 function checaVencedor(matriz) {
     let vencedor = false;
 
-    for (let linha = 0; linha < 3; linha++) {
+    for (let linha = 0; linha < matriz.length; linha++) {
         if (
             matriz[linha][0] != ' ' &&
             matriz[linha][0] === matriz[linha][1] &&
@@ -134,7 +186,7 @@ function checaVencedor(matriz) {
             vencedor = matriz[linha][0];
     }
 
-    for (let coluna = 0; coluna < 3; coluna++) {
+    for (let coluna = 0; coluna < matriz.length; coluna++) {
         if (
             matriz[0][coluna] != ' ' &&
             matriz[0][coluna] === matriz[1][coluna] &&
@@ -187,6 +239,7 @@ function main() {
             anunciaVencedor(vencedor, matriz, placar);
             if (jogarNovamente()) {
                 novaRodada(matriz, vencedor, placar);
+                indica_turno = (indica_turno % 2) + 1;
                 continue loopPrincipal;
             } else break loopPrincipal;
         }
